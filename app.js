7f2b9b34f2997a247b16ceb256d881ddbdd519c3
txusif <93634443+txusif@users.noreply.github.com>
2023-02-08 21:39:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomDiceImage1 = "dice" + randomNumber1 + ".png";
-var randomImageSource1 = "images/" + randomDiceImage1;
-var image1 = document.querySelectorAll(".img1")[0];
-image1.setAttribute("src", randomImageSource1);
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomDiceImage1 = `image/dice${randomNumber1}.png`;
+let image1 = document.getElementsByClassName("img1");
+image1.setAttribute("src", randomDiceImage1);
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomDiceImage2 = "dice" + randomNumber2 + ".png";
-var randomImageSource2 = "images/" + randomDiceImage2;
-var image2 = document.querySelectorAll(".img2")[0];
-image2.setAttribute("src", randomImageSource2);
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let randomDiceImage2 = `image/dice${randomNumber2}.png`;
+let image2 = document.getElementsByClassName("img2");
+image2.setAttribute("src", randomDiceImage2);
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩Player 1 wins!";
-}
-else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+} else if (randomNumber1 == randomNumber2) {
     document.querySelector("h1").innerHTML = "Draw!";
-}
-else {
-    document.querySelector("h1").innerHTML = "Player 2 wins!🚩";
+} else {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
 }
