d21c7c5c4eb78eadc75cc78b695ea1593eba2265
txusif <93634443+txusif@users.noreply.github.com>
2023-02-08 21:23:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage1 = "dice" + randomNumber1 + ".png";
-var randomImageSource1 = "Images/" + randomDiceImage1;
+var randomImageSource1 = "images/" + randomDiceImage1;
 var image1 = document.querySelectorAll(".img1")[0];
 image1.setAttribute("src", randomImageSource1);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
-var randomImageSource2 = "Images/" + randomDiceImage2;
+var randomImageSource2 = "images/" + randomDiceImage2;
 var image2 = document.querySelectorAll(".img2")[0];
 image2.setAttribute("src", randomImageSource2);
 
